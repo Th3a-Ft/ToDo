1467d599ec190b968bd1f24c179ba91aa782b31c
Théa <thea.fort@le-campus-numerique.fr>
2025-04-30 17:02:05 +0200

    Menu + déb form

---
+++ js/script.js
@@ -0,0 +1,97 @@
+const nbreJokes = 3;
+
+//Permet de consommer l'API
+async function fetchJokes() {
+  //Indique le nbre de fois recharger l'API et donc le nbre de fois afficher blaque
+  for (let i = 0; i < nbreJokes; i++) {
+    const reponse = await fetch("https://v2.jokeapi.dev/joke/Any?lang=fr");
+    const jokes = await reponse.json();  //await permet d'attendre le résultat de l'API avant de continuer la fct 
+    console.log(jokes.setup);
+    console.log(jokes.delivery);
+
+
+    //Affichage de l'API dans le html
+    //créer un élément div 
+    const dadJokes = document.createElement("div");
+    //on sélectionnne l'élement feed du HTML où sera affiché le feed
+    const cardFeed = document.getElementById("feed");
+
+    //affiche les élements dans la section dadjokes dans le html
+    dadJokes.innerHTML =
+      `<p>${jokes.setup}</p>
+      <p>${jokes.delivery}</p>
+      <hr>`
+
+    //ajoute l'élément enfant dadJokes à l'élément parent cardFeed
+    cardFeed.appendChild(dadJokes);
+
+  }
+}
+
+fetchJokes();
+
+
+
+//Menu dropdown
+const menu = document.createElement("div");
+const btnMenuDrop = document.getElementById("nav");
+const drop = document.getElementById("dropdown");
+
+function displayMenu() {
+  menu.innerHTML =
+    `<a href=#>Dropdown</a`
+  btnMenuDrop.appendChild(menu);
+}
+
+displayMenu();
+
+btnMenuDrop.addEventListener("click", function () {
+  drop.innerHTML =
+    `<a href="https://www.google.com/" target="_blank" alt="google.fr">Google</a>
+  <a href="https://www.google.com/" target="_blank" alt="google.fr">Google</a>
+  <a href="https://www.google.com/" target="_blank" alt="google.fr">Google</a>`
+
+  btnMenuDrop.appendChild(drop);
+});
+
+
+
+
+
+//Formulaire
+const form = document.getElementById("newForm");
+
+form.innerHTML =
+  `<h3>Formulaire</h3>
+  <label name="question">Question</label>
+  <textarea type="text" id="question" name="question"></textarea>
+  <label name="answer">Answer</label>
+  <textarea type="text" id="answer" name="answer"></textarea>
+  <button type="submit">Envoyer le message</button>`
+
+
+
+
+
+//Faire une card où la réponse au formulaire est affichée
+
+const myDadJokes = document.createElement("div");
+const cardJokes = document.getElementById("mesJokes")
+
+
+const question = document.getElementById("question").value
+const answer = document.getElementById("answer").value
+
+const myJokes = {
+  question: question,
+  answer: answer
+};
+
+
+cardJokes.innerHTML =
+  `<p>${myJokes.question}</p>
+    <p>${myJokes.answer}</p>`
+
+console.log(myJokes.question);
+console.log(myJokes.answer);
+console.log(myJokes);
