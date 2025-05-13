5250d49661eec7691791f180a0e4f74ecb945f6a
Th3a-Ft <thea.fort@le-campus-numerique.fr>
2025-05-13 16:54:11 +0200

    fin de journée

--- js/script.js
+++ js/script.js
@@ -15,3 +15,26 @@ btnDrop.addEventListener("click", function () {
 
 
 //Slider
+function displaySlider() {
+  const divSlider = document.createElement("div");
+  const slider = document.getElementById("slider");
+
+  const slides = ["img/ballon.jpg", "img/logo.png"];
+
+
+  for (let i=0;i<slides.length;i++){
+    //console.log(slides[1]);
+    divSlider.innerHTML =
+      `<img src=${slides[0]}>
+    `
+  };
+
+
+  
+  slider.appendChild(divSlider)
+  
+
+
+}
+
+setInterval("displaySlider(1)", 4000);
