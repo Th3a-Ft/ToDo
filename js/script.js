e582c38b33027c299c1596ebcfd6ea7e1f29c470
Th3a-Ft <thea.fort@le-campus-numerique.fr>
2025-05-14 10:01:22 +0200

    Carrousel updates

--- js/script.js
+++ js/script.js
@@ -15,26 +15,27 @@ btnDrop.addEventListener("click", function () {
 
 
 //Slider
-function displaySlider() {
-  const divSlider = document.createElement("div");
-  const slider = document.getElementById("slider");
+//Tableau où sont stockés les images
+const slides = ["img/ballon.jpg", "img/random.jpg","img/random2.jpg"];
+//Variable de l'index
+let currentIndex=0;
 
-  const slides = ["img/ballon.jpg", "img/logo.png"];
-
-
-  for (let i=0;i<slides.length;i++){
-    //console.log(slides[1]);
-    divSlider.innerHTML =
-      `<img src=${slides[0]}>
-    `
-  };
-
-
-  
-  slider.appendChild(divSlider)
-  
-
-
+//fonction pour afficher les images 
+function displaySlider(index) {
+  //récupère l'élément du HTML où afficher le slider
+  const sliderImg=document.getElementById("sliderImg")
+  //mise à jour de la source de l'image avec le chemin d'accès de l'image stocké ds le tableau
+  sliderImg.src=slides[index];
 }
 
-setInterval("displaySlider(1)", 4000);
+//fonction pour afficher la slide suivante
+function nextSlider(){
+  //incrémente la var currentIndex de 1 et vérifie que currentIndex est dans les limites du tableau
+  //ex si currentIndex=1 : (1+1) % 2 = 2 % 2 = 0 currentIndex devient = 0 car longueur du tableau est 2
+  currentIndex=(currentIndex+1)% slides.length;
+  displaySlider(currentIndex);
+}
+
+displaySlider(currentIndex);
+//la fonction nextSLider est appelée toutes les 4sec
+setInterval("nextSlider()", 4000);
